3cc552c9052d2c92ef557e1c602f3784711d4f40
sallespro <sallespro@gmail.com>
2021-01-15 09:01:22 -0300

    port

--- src/pages/image.js
+++ src/pages/image.js
@@ -1,21 +1,21 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-
 import Layout from "../components/Layout"
 
-/*
-  1. write query 
-  2. pass data into img component
-*/
 
 export const squareImage = graphql`
   fragment squareImage on File {
     childImageSharp {
-      fluid(maxWidth: 200, maxHeight: 200) {
-        ...GatsbyImageSharpFluid
+      fluid(maxWidth: 900, quality: 90, traceSVG: { color: "#2B2B2F" }) {
+        ...GatsbyImageSharpFluid_withWebp_tracedSVG
       }
     }
+    # childImageSharp {
+    #   fluid(maxWidth: 200, maxHeight: 200) {
+    #     ...GatsbyImageSharpFluid
+    #   }
+    # }
   }
 `
 
@@ -33,39 +33,48 @@ export default () => {
           node {
             name
             ...squareImage
-            # childImageSharp {
-            #   fluid(maxWidth: 915, quality: 70) {
-            #     aspectRatio
-            #     ...GatsbyImageSharpFluid_withWebp
-            #   }
-            # }
           }
         }
       }
       file(relativePath: { eq: "about.jpg" }) {
-        ...squareImage
-        # childImageSharp {
-        #   fluid {
-        #     base64
-        #     aspectRatio
-        #     src
-        #     srcSet
-        #     sizes
-        #   }
-        # }
+        childImageSharp {
+          fluid {
+            base64
+            aspectRatio
+            src
+            srcSet
+            sizes
+          }
+        }
       }
     }
   `)
   const pics = data.allFile.edges
   return (
     <Layout>
-      {pics.map(image => (
-        <Img
-          key={image.node.childImageSharp.fluid.src}
-          fluid={image.node.childImageSharp.fluid}
-          // style={{ margin: '3rem 0' }}
-        />
-      ))}
+      <div className="bg-gray-800 rounded-md p-4 max-w-lg w-full mx-auto mt-4">
+        <div className="flex space-x-12">
+          <div className="rounded-full bg-red-400 h-12 w-12"></div>
+
+          <button className="w-1/2 flex items-center justify-center rounded-md bg-red-400  transition duration-500 ease-in-out hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110"></button>
+          <div className="flex-1 space-y-2 py-1">
+            <div className="h-4 bg-red-400 rounded w-3/4"></div>
+            <div className="space-y-2">
+              {pics.map(image => (
+                <Img
+                  className="bg-red-400 rounded w-48  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+                  key={image.node.childImageSharp.fluid.src}
+                  fluid={image.node.childImageSharp.fluid}
+                  // style={{ margin: '3rem 0' }}
+                />
+              ))}
+              <div className="h-8 bg-red-400 rounded w-1/2"></div>
+            <div className="h-4 bg-red-400 rounded w-5/6"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* <Img fluid={data.file.childImageSharp.fluid} alt="about" /> */}
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,27 @@ const IndexPage = ({
   },
 }) => (
   <Layout>
+    <div className="bg-gray-800 rounded-md p-4 max-w-lg w-full mx-auto mt-4">
+      <div className="flex space-x-12">
+        <div className="rounded-full bg-red-400 h-12 w-12"></div>
+        
+        <button className="w-1/2 flex items-center justify-center rounded-md bg-red-400  transition duration-500 ease-in-out hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110">
+  
+</button>
+        <div className="flex-1 space-y-2 py-1">
+          <div className="h-4 bg-red-400 rounded w-3/4"></div>
+          <div className="space-y-2">
+            <div className="h-8 bg-red-400 rounded w-1/2"></div>
+            <div className="h-4 bg-red-400 rounded w-5/6"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    {/* <span className="flex h-3 w-3">
+  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-75"></span>
+  <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+</span> */}
     {/* {pages.map((page, index) => (
       <>
         <Link key={index} to={page.path}>{page.path}</Link>
@@ -22,7 +43,7 @@ export default IndexPage
 export const query = graphql`
   query IndexPageQuery {
     # allSitePage(filter: { path: { regex: "/services/" } }) {
-      allSitePage (sort: {fields: path, order: ASC}) {
+    allSitePage(sort: { fields: path, order: ASC }) {
       nodes {
         path
       }
